312dfa373d7f7b0d1262e8dc711917d24e73913c
Vinicius Kimura <viniciuskimura442@gmail.com>
2024-10-07 06:07:46 -0300

    Adding the openai.py file, where I put the request for OpenAI API to use the whisper-1 to transcript audio. Plan to use GPT as well soon

--- webhook.py
+++ webhook.py
@@ -1,31 +1,38 @@
 from flask import Flask, request, jsonify
 import os
-import requests
-import pygame
 import dotenv
+import gpt_api
+import meta_api
 
 dotenv.load_dotenv()
+list_id = [] #list containing the messages IDs
 
-whatsapp_token = os.environ.get("WHATSAPP_TOKEN")
+openai_client = gpt_api.initClient() #getting OPENAI client
+
 verify_token = os.environ.get("VERIFY_TOKEN")
 
 app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook_receiver():
     data = request.json  # Get the JSON data from the incoming request
-    # Process the data and perform actions based on the event
-    print("Received webhook data:", data)
-    print("\n")
-    message = data["entry"][0]["changes"][0]["value"]["messages"][0]
-    if(message["type"] == "audio"):
-        print(message["audio"]["id"])
-        url_request = requests.get(f'https://graph.facebook.com/v20.0/{message["audio"]["id"]}/', headers={'Authorization': f'Bearer {whatsapp_token}', 'Accept': 'application/json'})
-        url_request = url_request.json()['url']
-        print(url_request)
-
-        audio_request = requests.get(url_request, headers={'Authorization': f'Bearer {whatsapp_token}', 'Accept': 'application/json'})
-        print(audio_request)
-
+    #print("Received webhook data:", data)
+    #print("\n")
+    #Fist tests if the message is a message, if not, it would throw an error
+    try:
+        message = data["entry"][0]["changes"][0]["value"]["messages"][0]
+        audio_id = message["audio"]["id"]
+    except:
+        message = None
+    #Then it checks if the message is audio and if the message already got processed to prevent duplicates 
+    if(message != None and message["type"] == "audio" and audio_id not in list_id):
+        #print(audio_id)
+        list_id.append(audio_id) #Puts the message id on the list so it does not run again the same message
+        # Sends the audio id to a function that will return the audio byte data
+        audio_request = meta_api.audio_file(audio_id)
+        #With said byte information, we send it to my othor code to handle the OpenAI request
+        transcripted_text = gpt_api.transcript(openai_client, audio_request)
+        meta_api.send_message(transcripted_text)
+    
     return jsonify({'message': 'Webhook received successfully'}), 200
 
 @app.route('/webhook', methods=['GET'])
@@ -35,7 +42,7 @@ def verify():
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
     # Check if a token and mode were sent
-    if mode and token:
+    if mode and token: 
         # Check the mode and token sent are correct
         if mode == "subscribe" and token == verify_token:
             # Respond with 200 OK and challenge token from the request
